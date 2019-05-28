da5b296d6a31fb7a9f3e6924277ce939c9dfe09f
liuhuihui <244701374@qq.com>
2019-05-28 16:08:22 +0800

    添加字体图标 web字体

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="icon icon-A"></span>
+    <span class="text">ABSDFBD</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  import Epub from 'epubjs'
+  global.ePub = Epub
+  export default {
+    name: 'home'
   }
-}
 </script>
+
+<style scoped>
+  .text{
+    font-family: 'Days One';
+    font-size: 20px;
+    color: red;
+  }
+</style>
